0aa2aa5a349647871f96efb73201948e266611c1
orianac <orianac@uw.edu>
2021-01-22 19:21:31 -0800

    initial commit of summary tool

---
+++ catchment_analyses.py
@@ -0,0 +1,42 @@
+#!/bin/python
+
+
+import xarray as xr
+import sys
+import pandas as pd
+
+gcm_list = ['MIROC5', 'CanESM2', 'CCSM4', 'CNRM-CM5', 'CSIRO-Mk3-6-0',
+        'GFDL-ESM2M', 'HadGEM2-CC', 'HadGEM2-ES', 'inmcm4', 'IPSL-CM5A-MR']
+scenario_list = ['RCP45', 'RCP85']
+hydro_model_list = [('VIC', 'calib_inverse', 'P1'),
+                ('VIC', 'ORNL', 'P2'),
+                ('VIC', 'NCAR', 'P3'),
+                ('PRMS', 'calib_inverse', 'P1')]
+downscaling = 'maca'
+locations = pd.read_csv('./rupp_et_al_2020_locations.csv', header=None)[0].values
+
+
+for location_id in locations:
+    for gcm in gcm_list:
+        for scenario in scenario_list:
+            for (hydro_model, path_parameter_name, official_convention) in hydro_model_list:
+                file_path = '/pool0/data/orianac/bpa/output_files_from_hyak/output_files/merged.19500101-20991231.nc.{hydro_model}.{downscaling}.{scenario}.{path_parameter_name}.{gcm}'.format(hydro_model=hydro_model,
+                            downscaling=downscaling,
+                            scenario=scenario,
+                            path_parameter_name=path_parameter_name,
+                            gcm=gcm)
+                out_path = '/pool0/data/orianac/crcc/rupp_et_al_2020/catchment_mean_19500101-20991231_{scenario}_{gcm}_{downscaling}_{hydro_model}-{official_convention}.nc'.format(hydro_model=hydro_model,
+                            downscaling=downscaling,
+                            scenario=scenario,
+                            official_convention=official_convention,
+                            gcm=gcm)
+                try:
+                    print('Processing file {} for location {}'.format(file_path, location_id))
+                    data = xr.open_dataset(file_path)
+                    mask_path = '/pool0/data/orianac/FROM_RAID9/temp/remapped/remapUH_{}.nc'.format(location_id)
+                    mask = xr.open_dataset(mask_path).fraction
+                    data = data.where(mask)
+                    data.mean(dim=['lat', 'lon']).to_netcdf(out_path)
+                except:
+                    print('Oh no! Something failed for {} for location {}'.format(file_path, location_id))
+
